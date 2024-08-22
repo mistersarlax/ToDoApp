fdb70008b7170567cf9537e7e09d2ef8b911026e
Juan Sebastian Pinilla Giraldo <pinillagirladoj@gmail.com>
2024-08-22 12:32:49 -0500

    Create class Todo

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,7 @@
-# TODO: Add code here
+class Todo:
+    def __init__(self, code_id: int, title: str, description: str):
+        self.code_id: int = code_id
+        self.title: str = title
+        self.description: str = description
+        self.completed: bool = False
+        self.tags: list[str] = []
